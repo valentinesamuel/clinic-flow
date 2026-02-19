ab7c213229ffc7c3b9b98d10acbd70683e584ccb
valentinesamuel <valentinesamuel2580@gmail.com>
2026-02-19 13:35:01 +0100

    chore: replaced image log

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-959db468'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.l0hsmi47qd"
+    "revision": "0.jjcsjt0k33o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
